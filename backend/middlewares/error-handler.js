6508671498dd281cb9d75d5d2b6a494f3deca6bc
Shraddha <mailmeatshraddha@gmail.com>
2022-02-27 23:54:44 -0500

    fix: resolve Invalid status code error

--- backend/middlewares/error-handler.js
+++ backend/middlewares/error-handler.js
@@ -2,14 +2,13 @@
  * Handles all the errors across the application
  * @module middlewares/error-handler
  */
-const HTTP_INTERNAL_SERVER_ERROR = require('../utils/constants');
 
 const errorHandler = (err, req, res, next) => {
-  const { statusCode = HTTP_INTERNAL_SERVER_ERROR, message } = err;
+  const { status = 500, message } = err;
   res
-    .status(statusCode)
+    .status(status)
     .send({
-      message: statusCode === HTTP_INTERNAL_SERVER_ERROR
+      message: status === 500
         ? 'An error occurred on the server'
         : message,
     });
