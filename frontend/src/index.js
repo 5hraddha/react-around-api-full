000609b8313ab4f3269de73408f37417fe1a6b97
Shraddha <mailmeatshraddha@gmail.com>
2022-03-01 23:29:46 -0500

    fix: remove basename

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-around-auth">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
